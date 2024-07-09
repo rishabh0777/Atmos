657b138083d650980af5932d60b2f9131c538343
rishabh0777 <rishabhsrivastava7777@gmail.com>
2024-07-09 17:32:13 +0530

    Client Side routing bug fixing

--- src/components/Right.jsx
+++ src/components/Right.jsx
@@ -1,22 +1,19 @@
-import React from 'react'
-import TodayForecast from './TodayForecast'
-import FiveDaysForecast from './FiveDaysForecast'
-import {  Routes, Route, Navigate } from 'react-router-dom'
+// src/components/Right.jsx
+import React from 'react';
+import TodayForecast from './TodayForecast';
+import FiveDaysForecast from './FiveDaysForecast';
+import { Routes, Route, Navigate } from 'react-router-dom';
 
 const Right = () => {
   return (
     <div className='w-[72%] h-screen bg-[#1b0951] text-white xsm:w-full xsm:h-screen'>
-      
-    <Routes>
-      <Route path='/today'
-      element = {<TodayForecast />}/>
-      <Route path='/5days'
-      element={<FiveDaysForecast />}/>
-      <Route path='/*'
-      element={<Navigate  to='/today' replace/>}/>
-    </Routes>
+      <Routes>
+        <Route path='/today' element={<TodayForecast />} />
+        <Route path='/5days' element={<FiveDaysForecast />} />
+        <Route path='/*' element={<Navigate to='/today' replace />} />
+      </Routes>
     </div>
-  )
-}
+  );
+};
 
-export default Right
+export default Right;
